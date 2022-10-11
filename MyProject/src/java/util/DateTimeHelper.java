07fab34572f0f07d658aa538783d9f4190d77cf7
bahoann79 <89914081+bahoann79@users.noreply.github.com>
2022-10-11 09:12:06 +0700

    Create DateTimeHelper.java
    viết function xử lý ngày giờ của Day Of Week để sử dụng thẻ select thông qua listDayOfWeek

---
+++ MyProject/src/java/util/DateTimeHelper.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author admin
+ */
+public class DateTimeHelper {
+
+    public static String getDayTomorrow(String day) {
+
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("dd/MM");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(format.parse(day));
+            calendar.add(Calendar.DATE, 1);
+            String result = format.format(calendar.getTime());
+            return result;
+
+        } catch (ParseException ex) {
+            Logger.getLogger(DateTimeHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static boolean currentDateInRange(String startDate, String endDate) {
+        startDate += "/2022";
+        endDate += "/2022";
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            Date currentDate = new Date();
+            Date dateFrom = formatter.parse(startDate);
+            Date dateTo = formatter.parse(endDate);
+            if (currentDate.after(dateFrom) && currentDate.before(dateTo)) {
+                return true;
+            }
+        } catch (ParseException ex) {
+            Logger.getLogger(DateTimeHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+
+    }
+}
