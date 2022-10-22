42fbc358599d6a8d981c0f0c09f89c7128b3edb3
bahoann79 <89914081+bahoann79@users.noreply.github.com>
2022-10-22 16:26:03 +0700

    HomeController
    Lấy dữ liệu từ DB -> đẩy sang phía client để hiển hị

--- MyProject/src/java/controller/lecturer/LecturerViewHome.java
+++ MyProject/src/java/controller/lecturer/LecturerViewHome.java
@@ -4,12 +4,22 @@
  */
 package controller.lecturer;
 
+import dal.LecturerDBContext;
+import dal.SessionDBContext;
+import dal.TimeSlotDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import model.Lecturer;
+import model.Session;
+import model.TimeSlot;
 
 
 /**
@@ -19,16 +29,43 @@ import java.io.PrintWriter;
 public class LecturerViewHome extends HttpServlet {
 
  
+   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int lecturerId = Integer.parseInt(request.getParameter("lecturerId"));
+
+        Date today = Date.valueOf(LocalDate.now());
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String stToday = formatter.format(today);
+        request.setAttribute("today", stToday);
+
+        TimeSlotDBContext tlDB = new TimeSlotDBContext();
+        ArrayList<TimeSlot> timeSlots = tlDB.list();
+        request.setAttribute("timeSlots", timeSlots);
+
+        SessionDBContext sesDB = new SessionDBContext();
+        ArrayList<Session> sessions = sesDB.filterHome(lecturerId, today);
+        Integer size = sessions.size();
+        request.setAttribute("sessions", sessions);
+        request.setAttribute("size", size);
+
+        LecturerDBContext lecDB = new LecturerDBContext();
+        Lecturer lecturer = lecDB.get(lecturerId);
+        request.setAttribute("lecturer", lecturer);
+
+        request.getRequestDispatcher("../view/lecturer/home.jsp").forward(request, response);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.sendRedirect("../view/lecturer/home.jsp");
+        processRequest(request, response);
     }
 
-   
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        processRequest(request, response);
     }
 
   
