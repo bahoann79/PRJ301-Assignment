187ebbe2b68e068752ed5403c7b67333c4c9eb5e
bahoann79 <89914081+bahoann79@users.noreply.github.com>
2022-10-31 15:51:07 +0700

    Update SessionDBContext.java
    create function filterViewAttendance để lấy ra 1 list các session của học sinh trong 1 lớp với các tham số truyền vào là studentId & groupId

--- MyProject/src/java/dal/SessionDBContext.java
+++ MyProject/src/java/dal/SessionDBContext.java
@@ -27,6 +27,95 @@ import model.TimeSlot;
 public class SessionDBContext extends DBContext<Session> {
 
     /*
+        for student view attendance
+     */
+    public ArrayList<Session> filterStViewAttendance(int studentId, int groupId) {
+        try {
+            ArrayList<Session> listSession = new ArrayList<>();
+
+            String sql = "SELECT s.session_id, s.[date], s.[index], s.attended,\n"
+                    + "	   st.student_id, st.student_code, st.full_name,\n"
+                    + "	   ISNULL(a.[status], 0) [status], ISNULL(a.[description], '') [description],\n"
+                    + "	   lec.lecturer_id, lec.lecturer_code,\n"
+                    + "	   gr.group_id, gr.group_name,\n"
+                    + "	   sub.subject_id, sub.subject_code, sub.subject_name,\n"
+                    + "	   r.room_id, r.room_name,\n"
+                    + "	   tl.time_slot_id, tl.t_description	\n"
+                    + "FROM [Session] s INNER JOIN [Group] gr ON s.group_id = gr.group_id\n"
+                    + "				 INNER JOIN StudentGroup sg ON sg.group_id = gr.group_id\n"
+                    + "				 INNER JOIN Student st ON st.student_id = sg.student_id\n"
+                    + "				 LEFT JOIN Attendance a ON a.student_id = st.student_id AND a.session_id = s.session_id\n"
+                    + "				 INNER JOIN Lecturer lec ON lec.lecturer_id = s.lecturer_id\n"
+                    + "				 INNER JOIN [Subject] sub ON sub.subject_id = gr.subject_id\n"
+                    + "				 INNER JOIN Room r ON s.room_id = r.room_id\n"
+                    + "				 INNER JOIN TimeSlot tl ON s.time_slot_id = tl.time_slot_id\n"
+                    + "				 WHERE st.student_id = ? AND gr.group_id = ?";
+
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, studentId);
+            stm.setInt(2, groupId);
+
+            ResultSet rs = stm.executeQuery();
+
+            while (rs.next()) {
+                Session session = new Session();
+                Student student = new Student();
+                Attendance attendance = new Attendance();
+                Lecturer lecturer = new Lecturer();
+                Group group = new Group();
+                Subject subject = new Subject();
+                Room room = new Room();
+                TimeSlot timeSlot = new TimeSlot();
+
+                session.setSessionId(rs.getInt("session_id"));
+                session.setDate(rs.getDate("date"));
+                session.setIndex(rs.getInt("index"));
+                session.setAttended(rs.getBoolean("attended"));
+
+                student.setStudentId(rs.getInt("student_id"));
+                student.setStudentCode(rs.getString("student_code"));
+                student.setFullName(rs.getString("full_name"));
+                group.getStudents().add(student);
+
+                attendance.setStudent(student);
+                attendance.setSession(session);
+                attendance.setStatus(rs.getBoolean("status"));
+                attendance.setDescription(rs.getString("description"));
+                session.getAttendances().add(attendance);
+
+                lecturer.setLecturerId(rs.getInt("lecturer_id"));
+                lecturer.setLecturerCode(rs.getString("lecturer_code"));
+                session.setLecturer(lecturer);
+
+                subject.setSubjectId(rs.getInt("subject_id"));
+                subject.setSubjectCode(rs.getString("subject_code"));
+                subject.setSubjectName(rs.getString("subject_name"));
+                group.setSubject(subject);
+
+                group.setGroupId(rs.getInt("group_id"));
+                group.setGroupName(rs.getString("group_name"));
+                session.setGroup(group);
+
+                room.setRoomId(rs.getInt("room_id"));
+                room.setRoomName(rs.getString("room_name"));
+                session.setRoom(room);
+
+                timeSlot.setTimeSlotId(rs.getInt("time_slot_id"));
+                timeSlot.setDescription(rs.getString("t_description"));
+                session.setTimeSlot(timeSlot);
+
+                listSession.add(session);
+
+            }
+            return listSession;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    /*
         for lecturer schedule
      */
     public ArrayList<Session> filterSchedule(int sessionId, Date from, Date to) {
